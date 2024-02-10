08271cbcbb79ea5b90f85ef4f7e23af287890697
Patrik Cihal <patrik.cihal1234@gmail.com>
2024-02-10 19:13:22 +0100

    Snake _ case

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,12 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput, Data, DataEnum};
+use convert_case::{Case, Casing};
 
 #[proc_macro_derive(Textures)]
 pub fn textures(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    
+
     let name = input.ident; // The name of the enum
     let data_enum = if let Data::Enum(data_enum) = input.data {
         data_enum
@@ -18,7 +19,7 @@ pub fn textures(input: TokenStream) -> TokenStream {
 
     let variants = data_enum.variants.iter().map(|variant| {
         let variant_name = &variant.ident;
-        let file_name = format!("/assets/textures/{}.png", variant_name.to_string().to_lowercase());
+        let file_name = format!("/assets/textures/{}.png", variant_name.to_string().to_case(Case::Snake));
         quote! {
             #name::#variant_name => include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), #file_name)).to_vec(),
         }
